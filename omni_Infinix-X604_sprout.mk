539cf0f65ab26cc46ce224b32bac7f8f0d92fbf4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-03-03 00:41:24 +0100

    Infinix-X604_sprout: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: infinix Model: Infinix X604 Script version: 3.0.0

---
+++ omni_Infinix-X604_sprout.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from Infinix-X604_sprout device
+$(call inherit-product, device/infinix/Infinix-X604_sprout/device.mk)
+
+PRODUCT_DEVICE := Infinix-X604_sprout
+PRODUCT_NAME := omni_Infinix-X604_sprout
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X604
+PRODUCT_MANUFACTURER := infinix
+
+PRODUCT_GMS_CLIENTID_BASE := android-transsion-infinix-rev1
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_x604_h633-user 10 QP1A.190711.020 168928 release-keys"
+
+BUILD_FINGERPRINT := Infinix/H633/Infinix-X604_sprout:10/QP1A.190711.020/H-210930V403:user/release-keys
