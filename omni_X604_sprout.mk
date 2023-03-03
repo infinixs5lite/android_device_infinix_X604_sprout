6241c80744c527970a04d1b1bae0b531d1706bf8
infinixs5lite <kelvinchinedu97@gmail.com>
2023-03-03 11:12:06 +0100

    Update omni_X604_sprout.mk

--- omni_X604_sprout.mk
+++ omni_X604_sprout.mk
@@ -6,8 +6,9 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
