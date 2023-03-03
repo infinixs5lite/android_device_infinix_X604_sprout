04b5f5379291ded197d3322d481a745308600dc9
infinixs5lite <kelvinchinedu97@gmail.com>
2023-03-03 10:57:39 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/infinix/X604
+DEVICE_PATH := device/infinix/X604_sprout
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
